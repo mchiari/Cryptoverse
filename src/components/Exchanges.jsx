507ef693e9e11406140c28922ddb92f588208960
mchiari <mchiaridev@gmail.com>
2022-03-02 20:55:01 -0300

    first commit

---
+++ src/components/Exchanges.jsx
@@ -0,0 +1,11 @@
+import React from 'react'
+
+const Exchanges = () => {
+  return (
+    <div>
+      Exchanges - need premium plan on api to use this - so forget about it ):
+    </div>
+  )
+}
+
+export default Exchanges
